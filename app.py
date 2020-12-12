e11a37f03d646befd114a7f85bc0cbd0dbe33f3f
anwaruddin08 <anwaruddin08@users.noreply.github.com>
2020-12-12 20:54:35 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask, render_template, request, redirect
+
+from inference import get_prediction
+from commons import format_class_name
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        img_bytes = file.read()
+        class_name = get_prediction(image_bytes=img_bytes)
+        return jsonify({'class_name': class_name})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
---
+++ commons.py
@@ -0,0 +1,29 @@
+import io
+
+
+from PIL import Image
+import torch as pt
+import torchvision.transforms as transforms
+
+
+def get_model():
+    model = pt.load("super_model.pth", map_location=pt.device("cpu"))
+    model.eval()
+    return model
+
+
+def transform_image(image_bytes):
+   transforms.Compose([
+        transforms.Resize(200),          
+        transforms.CenterCrop(300),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    image = Image.open(io.BytesIO(image_bytes))
+    return my_transforms(image).unsqueeze(0)
+
+
+def format_class_name(class_name):
+    class_name = class_name.replace('_', ' ')
+    class_name = class_name.title()
+    return class_name
