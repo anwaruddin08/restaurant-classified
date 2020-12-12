8d57c29e8bca2c6072a052d8a56074e932279ee1
anwaruddin08 <anwaruddin08@users.noreply.github.com>
2020-12-12 21:14:47 +0700

    Update commons.py

--- commons.py
+++ commons.py
@@ -13,12 +13,12 @@ def get_model():
 
 
 def transform_image(image_bytes):
-     my_transforms = transforms.Compose([
-        transforms.Resize(200),          
-        transforms.CenterCrop(300),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+     my_transforms = transforms.Compose([transforms.Resize(200),          
+                                        transforms.CenterCrop(300),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(
+                                            [0.485, 0.456, 0.406], 
+                                            [0.229, 0.224, 0.225])])
     image = Image.open(io.BytesIO(image_bytes))
     return my_transforms(image).unsqueeze(0)
 
